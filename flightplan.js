61fbe34a5ab67d7201b36266d1cfc94d8c8a721d
Jill Magsaysay <jmagsaysay@10-4.com>
2016-06-06 13:44:04 -0600

    flightplan

--- flightplan.js
+++ flightplan.js
@@ -1,26 +1,16 @@
 var plan = require('flightplan');
 
-var appName = 'jillian_magsaysay_site';
-var username = 'admin';
+var appName = 'mysite';
+var username = 'jill';
 var startFile = 'src/www';
 
 var tmpDir = appName+'-' + new Date().getTime();
-
-// configuration
-plan.target('staging', [
-  {
-    host: '162.243.2.242',
-    username: username,
-    privateKey: '/root/.ssh/id_rsa',
-    agent: process.env.SSH_AUTH_SOCK
-  }
-]);
 
 plan.target('production', [
   {
     host: '162.243.2.242',
     username: username,
-
+    privateKey: '/root/.ssh/id_rsa',
     agent: process.env.SSH_AUTH_SOCK
   },
 //add in another server if you have more than one
