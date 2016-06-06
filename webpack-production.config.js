d7054afac5db38e7257c40bbdb222f983c3ea186
Jill Magsaysay <jmagsaysay@10-4.com>
2016-06-06 05:44:11 -0600

    prod config

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -29,12 +29,41 @@ const config = {
         from: 'www'
     }, ], path.resolve(__dirname, 'src')), ],
     module: {
-        loaders: [{
-            test: /\.js$/, // All .js files
-            loaders: ['babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
-            exclude: [nodeModulesPath],
-        }, ],
+      preLoaders: [
+          {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'source-map'
+          }
+      ],
+      loaders: [
+          {
+              test: /\.scss$/,
+              include: /src/,
+              loaders: [
+                  'style',
+                  'css',
+                  'autoprefixer?browsers=last 3 versions',
+                  'sass?outputStyle=expanded'
+              ]
+          },
+          {
+              test: /\.(jpe?g|png|gif|svg)$/i,
+              loaders: [
+                  'url?limit=8192',
+                  'img'
+              ]
+          },
+          {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loaders: [
+                  'react-hot',
+                  'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
+              ]
+          }
+      ],
     },
 };
 
-module.exports = config; 
+module.exports = config;
