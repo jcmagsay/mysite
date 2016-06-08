a00a4ee9a23a4550218847f76e18454c744af838
Jill Magsaysay <jmagsaysay@10-4.com>
2016-06-07 22:40:58 -0600

    verified -- stable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main';
+import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {Router, Route, IndexRoute, hashHistory } from 'react-router'
+import About from './components/About';
+import Process from './components/Process';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Layout from './components/Layout';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// Render the main app react component into the app div.
+// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+//render(<Main />, document.getElementById('app'));
+// render(<Navigation />, document.getElementById('navigation'));
+// render(<Container />, document.getElementById('about'));
+
+const app = document.getElementById('app');
+
+render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={About}></IndexRoute>
+      <Route path="about" component={About}></Route>
+      <Route path="process" component={Process}></Route>
+      <Route path="projects" component={Projects}></Route>
+      <Route path="contact" component={Contact}></Route>
+    </Route>
+  </Router>,
+  app
+);
