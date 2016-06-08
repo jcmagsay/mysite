79edaa289b88dc671a6bda63431e1a58ff65f189
Jill Magsaysay <jmagsaysay@10-4.com>
2016-06-07 22:31:35 -0600

    fixing project -- stable

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
+import 'core-js/fn/object/assign';
 import React from 'react';
-import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import {Router, Route, IndexRoute, hashHistory } from 'react-router'
-import About from './components/About';
-import Process from './components/Process';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Layout from './components/Layout';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
-const app = document.getElementById('app');
+import ReactDOM from 'react-dom';
+import App from './components/Main';
 
 // Render the main component into the dom
-render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={About}></IndexRoute>
-      <Route path="about" component={About}></Route>
-      <Route path="process" component={Process}></Route>
-      <Route path="projects" component={Projects}></Route>
-      <Route path="contact" component={Contact}></Route>
-    </Route>
-  </Router>,
-  app
-);
+ReactDOM.render(<App />, document.getElementById('app'));
